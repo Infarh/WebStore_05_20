4b1dc52697a356bed70c0bd25faae1084b1d7703
Infarh <shmachilin@yandex.ru>
2020-05-15 20:18:01 +0300

    DTO-модели для процессе передачи данных в WebAPI каталога товаров

---
+++ Common/WebStore.Domain/DTO/Products/BrandDTO.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.DTO.Products
+{
+    public class BrandDTO : INamedEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
