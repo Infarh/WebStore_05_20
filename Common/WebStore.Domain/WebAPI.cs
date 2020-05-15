45f06219dab68725af3a26df903b76211f275912
Infarh <shmachilin@yandex.ru>
2020-05-15 21:02:07 +0300

    Контроллер WebAPI сервиса заказов

--- Common/WebStore.Domain/WebAPI.cs
+++ Common/WebStore.Domain/WebAPI.cs
@@ -7,5 +7,7 @@
         public const string Employees = "api/v1/employees";
 
         public const string Products = "api/v1/products";
+
+        public const string Orders = "api/v1/orders";
     }
 }
---
+++ Services/WebStore.ServiceHosting/Controllers/OrdersApiController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Orders;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Orders)]
+    [ApiController]
+    public class OrdersApiController : ControllerBase, IOrderService
+    {
+        private readonly IOrderService _OrderService;
+
+        public OrdersApiController(IOrderService OrderService) => _OrderService = OrderService;
+
+        [HttpGet("user/{UserName}")]
+        public IEnumerable<OrderDTO> GetUserOrders(string UserName) => _OrderService.GetUserOrders(UserName);
+
+        [HttpGet("{id}")]
+        public OrderDTO GetOrderById(int id) => _OrderService.GetOrderById(id);
+
+        [HttpPost("{UserName}")]
+        public Task<OrderDTO> CreateOrderAsync(string UserName, /*[FromBody] */CreateOrderModel OrderModel) => _OrderService.CreateOrderAsync(UserName, OrderModel);
+    }
+}
