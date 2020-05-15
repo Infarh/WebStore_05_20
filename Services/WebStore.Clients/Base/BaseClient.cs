554735c84e1c1151e7e83edc1ae2a1f12be2dae9
Infarh <shmachilin@yandex.ru>
2020-05-15 20:43:21 +0300

    Клиент WebAPI каталога товаров

--- Services/WebStore.Clients/Base/BaseClient.cs
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -29,15 +29,15 @@ namespace WebStore.Clients.Base
 
         }
 
-        protected T Get<T>(string url) where T : new() => GetAsync<T>(url).Result;
+        protected T Get<T>(string url) /*where T : new()*/ => GetAsync<T>(url).Result;
 
-        protected async Task<T> GetAsync<T>(string url, CancellationToken Cancel = default) where T : new()
+        protected async Task<T> GetAsync<T>(string url, CancellationToken Cancel = default)// where T : new()
         {
             var response = await _Client.GetAsync(url, Cancel);
-            if (response.IsSuccessStatusCode)
-                return await response.Content.ReadAsAsync<T>(Cancel);
-
-            return new T();
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>(Cancel);
+            //if (response.IsSuccessStatusCode)
+            //    return await response.Content.ReadAsAsync<T>(Cancel);
+            //return new T();
         }
 
         protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
---
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Products
+{
+    public class ProductsClient : BaseClient, IProductData
+    {
+        public ProductsClient(IConfiguration Configuration) : base(Configuration, WebAPI.Products) { }
+
+        public IEnumerable<Section> GetSections() => Get<IEnumerable<Section>>($"{_ServiceAddress}/sections");
+
+        public IEnumerable<Brand> GetBrands() => Get<IEnumerable<Brand>>($"{_ServiceAddress}/brands");
+
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null) =>
+            Post(_ServiceAddress, Filter ?? new ProductFilter())
+               .Content
+               .ReadAsAsync<IEnumerable<ProductDTO>>()
+               .Result;
+
+        public ProductDTO GetProductById(int id) => Get<ProductDTO>($"{_ServiceAddress}/{id}");
+    }
+}
