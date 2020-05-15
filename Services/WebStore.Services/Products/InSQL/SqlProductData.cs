44ec257813549af97826d8ee2756438a8f5e9298
Infarh <shmachilin@yandex.ru>
2020-05-15 21:19:33 +0300

    Расчищаем грабли

--- Services/WebStore.Services/Products/InSQL/SqlProductData.cs
+++ Services/WebStore.Services/Products/InSQL/SqlProductData.cs
@@ -16,16 +16,18 @@ namespace WebStore.Services.Products.InSQL
         public SqlProductData(WebStoreDB db) => _db = db;
 
         public IEnumerable<Section> GetSections() => _db.Sections
-           .Include(section => section.Products)
+           //.Include(section => section.Products)
            .AsEnumerable();
 
         public IEnumerable<Brand> GetBrands() => _db.Brands
-           .Include(brand => brand.Products)
+           //.Include(brand => brand.Products)
            .AsEnumerable();
 
         public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null)
         {
-            IQueryable<Product> query = _db.Products;
+            IQueryable<Product> query = _db.Products
+               .Include(product => product.Section)
+               .Include(product => product.Brand);
 
             if (Filter?.BrandId != null)
                 query = query.Where(product => product.BrandId == Filter.BrandId);
