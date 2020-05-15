592b6f0e854535b49ce1a793f02d5b014f621e58
Infarh <shmachilin@yandex.ru>
2020-05-15 20:27:12 +0300

    Проекции данных для DTO моделей товаров, секций, брендов

---
+++ Services/WebStore.Services/Mapping/BrandMapping.cs
@@ -0,0 +1,20 @@
+﻿using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Mapping
+{
+    public static class BrandMapping
+    {
+        public static BrandDTO ToDTO(this Brand Brand) => Brand is null ? null : new BrandDTO
+        {
+            Id = Brand.Id,
+            Name = Brand.Name
+        };
+
+        public static Brand FromDTO(this BrandDTO Brand) => Brand is null ? null : new Brand
+        {
+            Id = Brand.Id,
+            Name = Brand.Name
+        };
+    }
+}
---
+++ Services/WebStore.Services/Mapping/SectionMapping.cs
@@ -0,0 +1,20 @@
+﻿using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Mapping
+{
+    public static class SectionMapping
+    {
+        public static SectionDTO ToDTO(this Section Section) => Section is null ? null : new SectionDTO
+        {
+            Id = Section.Id,
+            Name = Section.Name
+        };
+
+        public static Section FromDTO(this SectionDTO Section) => Section is null ? null : new Section
+        {
+            Id = Section.Id,
+            Name = Section.Name
+        };
+    }
+}
