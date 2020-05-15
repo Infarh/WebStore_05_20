6a918695b25a806adb447adf4108b341a751326e
Infarh <shmachilin@yandex.ru>
2020-05-15 19:48:11 +0300

    Добавляем класс в котором все адреса нашего API будут храниться в виде констант

---
+++ Common/WebStore.Domain/WebAPI.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStore.Domain
+{
+    public static class WebAPI
+    {
+        public const string Values = "api/values";
+
+        public const string Employees = "api/employees";
+    }
+}
