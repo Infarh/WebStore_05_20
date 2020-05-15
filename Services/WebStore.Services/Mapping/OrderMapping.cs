f0fcfec42e8e289e2a6f6046445d96171f847129
Infarh <shmachilin@yandex.ru>
2020-05-15 20:53:41 +0300

    Проекции данных для DTO-моделей заказов

---
+++ Services/WebStore.Services/Mapping/OrderMapping.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using WebStore.Domain.DTO.Orders;
+using WebStore.Domain.Entities.Orders;
+
+namespace WebStore.Services.Mapping
+{
+    public static class OrderMapping
+    {
+        public static OrderDTO ToDTO(this Order order) => order is null ? null : new OrderDTO
+        {
+            Id = order.Id,
+            Phone = order.Phone,
+            Address = order.Address,
+            Date = order.Date,
+            OrderItems = order.OrderItems.Select(ToDTO)
+        };
+
+        public static OrderItemDTO ToDTO(this OrderItem item) => item is null ? null : new OrderItemDTO
+        {
+            Id = item.Id,
+            Price = item.Price,
+            Quantity = item.Quantity
+        };
+
+        public static Order FromDTO(this OrderDTO order) => order is null ? null : new Order
+        {
+            Id = order.Id,
+            Phone = order.Phone,
+            Address = order.Address,
+            Date = order.Date,
+            OrderItems = order.OrderItems.Select(FromDTO).ToArray()
+        };
+
+        public static OrderItem FromDTO(this OrderItemDTO item) => item is null ? null : new OrderItem
+        {
+            Id = item.Id,
+            Price = item.Price,
+            Quantity = item.Quantity
+        };
+    }
+}
