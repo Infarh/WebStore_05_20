4453f2037b5ecc503df0dd57e0238b01cef57af5
Infarh <shmachilin@yandex.ru>
2020-05-15 21:06:02 +0300

    Регистрируем клиента WebAPI сервиса заказов

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WebStore.Clients.Employees;
+using WebStore.Clients.Orders;
 using WebStore.Clients.Products;
 using WebStore.Clients.Values;
 using WebStore.DAL.Context;
@@ -73,7 +74,8 @@ namespace WebStore
             //services.AddScoped<IProductData, SqlProductData>();
             services.AddScoped<IProductData, ProductsClient>();
             services.AddScoped<ICartService, CookiesCartService>();
-            services.AddScoped<IOrderService, SqlOrderService>();
+            //services.AddScoped<IOrderService, SqlOrderService>();
+            services.AddScoped<IOrderService, OrdersClient>();
 
             services.AddScoped<IValueServices, ValuesClient>();
         }
