817142cf8620b1859615d380967fcd716a81a9b2
Infarh <shmachilin@yandex.ru>
2020-05-19 19:20:04 +0300

    Интерфйейсы для WebAPI системы Identity

---
+++ Services/WebStore.Interfaces/Services/IUserClient.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Interfaces.Services
+{
+    public interface IUserClient :
+        IUserRoleStore<User>,
+        IUserPasswordStore<User>,
+        IUserEmailStore<User>,
+        IUserPhoneNumberStore<User>,
+        IUserTwoFactorStore<User>,
+        IUserLockoutStore<User>,
+        IUserClaimStore<User>,
+        IUserLoginStore<User>
+    {
+    }
+}
