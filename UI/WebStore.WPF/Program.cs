745aab3faf681c8bba702782a2dfe505d7af2fd0
Infarh <shmachilin@yandex.ru>
2020-05-21 15:36:43 +0300

    Добавляем свою реализацию точки входа в приложение (метод Main)

---
+++ UI/WebStore.WPF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebStore.WPF
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
