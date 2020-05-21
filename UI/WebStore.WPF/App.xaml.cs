ee12792123cc159edc30ba4f01e82dd29f55767c
Infarh <shmachilin@yandex.ru>
2020-05-21 15:43:54 +0300

    Создаём, запускаем и останавливаем хост WPF-приложения

--- UI/WebStore.WPF/App.xaml.cs
+++ UI/WebStore.WPF/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.Hosting;
 
 namespace WebStore.WPF
 {
@@ -7,11 +9,30 @@ namespace WebStore.WPF
         private static bool __IsDesignTime = true;
         public static bool IsDesignTime => __IsDesignTime;
 
-        protected override void OnStartup(StartupEventArgs e)
+        private static IHost __Host;
+
+        public static IHost Host => __Host ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        protected override async void OnStartup(StartupEventArgs e)
         {
             __IsDesignTime = false;
 
+            var host = Host;
+
+
             base.OnStartup(e);
+
+            await host.StartAsync().ConfigureAwait(false);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            var host = Host;
+            await host.StopAsync().ConfigureAwait(false);
+            host.Dispose();
+            __Host = null;
         }
     }
 }
