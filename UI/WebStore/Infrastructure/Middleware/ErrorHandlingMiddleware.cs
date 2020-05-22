0dd4d2697926dee0779634e81e1625e04aceaa8d
Infarh <shmachilin@yandex.ru>
2020-05-22 21:22:57 +0300

    Добавляем промежуточное ПО, обеспечивающее обработку ошибок и логирование в конвейере обработки входящих запросов

---
+++ UI/WebStore/Infrastructure/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace WebStore.Infrastructure.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _Next;
+        private readonly ILogger<ErrorHandlingMiddleware> _Logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate Next, ILogger<ErrorHandlingMiddleware> Logger)
+        {
+            _Next = Next;
+            _Logger = Logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _Next(context);
+            }
+            catch (Exception e)
+            {
+                HandleException(context, e);
+                throw;
+            }
+        }
+
+        private void HandleException(HttpContext Context, Exception Error)
+        {
+            _Logger.LogError(Error, "Ошибка при обработке запроса {0}", Context.Request.Path);
+        }
+    }
+}
--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -14,6 +14,7 @@ using WebStore.Clients.Products;
 using WebStore.Clients.Values;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
+using WebStore.Infrastructure.Middleware;
 using WebStore.Interfaces.Api;
 using WebStore.Interfaces.Services;
 using WebStore.Logger;
@@ -107,6 +108,8 @@ namespace WebStore
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseEndpoints(endpoints =>
             {
