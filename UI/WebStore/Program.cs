e8a5d49cff7e19aa0c467ac038f4ceb21390afe5
Infarh <shmachilin@yandex.ru>
2020-05-22 19:47:50 +0300

    Конфигурирование системы логирования в коде

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -1,5 +1,8 @@
+using System.Security.Cryptography;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace WebStore
 {
@@ -12,6 +15,20 @@ namespace WebStore
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((host, log) =>
+                {
+                    //log.ClearProviders();
+                    //log.AddProvider(new ConsoleLoggerProvider())
+                    //log.AddConsole(opt => opt.IncludeScopes = true);
+                    //log.AddEventLog(opt => opt.LogName = "WebStore");
+                    //log.AddFilter("System", LogLevel.Warning);
+                    //log.AddFilter((category, level) =>
+                    //{
+                    //    if (category.StartsWith("Microsoft")) 
+                    //        return level >= LogLevel.Warning;
+                    //    return true;
+                    //});
+                })
                 .ConfigureWebHostDefaults(builder =>
                 {
                     builder.UseStartup<Startup>();
