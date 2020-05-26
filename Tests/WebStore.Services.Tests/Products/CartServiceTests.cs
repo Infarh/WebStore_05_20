d5f21dc4b48ec1dfa23ebfbfc73d9af68f203dae
Infarh <shmachilin@yandex.ru>
2020-05-26 20:58:56 +0300

    Тестирование модели корзины и модели-представления корзины

---
+++ Tests/WebStore.Services.Tests/Products/CartServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebStore.Domain.Models;
+using WebStore.Domain.ViewModels;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Services.Tests.Products
+{
+    [TestClass]
+    public class CartServiceTests
+    {
+        [TestMethod]
+        public void Cart_Class_ItemsCount_returns_Correct_Quantity()
+        {
+            var cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new CartItem { ProductId = 1, Quantity = 1 },
+                    new CartItem { ProductId = 2, Quantity = 3 },
+                }
+            };
+            const int expected_count = 4;
+
+            var actual_count = cart.ItemsCount;
+
+            Assert.Equal(expected_count, actual_count);
+        }
+
+        [TestMethod]
+        public void CartViewModel_Returns_Correct_ItemsCount()
+        {
+            var cart_view_model = new CartViewModel
+            {
+                Items = new Dictionary<ProductViewModel, int>
+                {
+                    { new ProductViewModel {Id = 1, Name = "Product 1", Price = 0.5m}, 1 },
+                    { new ProductViewModel {Id = 2, Name = "Product 2", Price = 1.5m}, 3 },
+                }
+            };
+            const int expected_count = 4;
+
+            var actual_count = cart_view_model.ItemsCount;
+
+            Assert.Equal(expected_count, actual_count);
+        }
+    }
+}
