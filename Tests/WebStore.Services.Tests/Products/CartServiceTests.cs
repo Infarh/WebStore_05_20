a30d5e0e3662ca7b8c8da45b6a04c7d121dc31ba
Infarh <shmachilin@yandex.ru>
2020-05-26 21:02:00 +0300

    Подготовка к тестированию корзины

--- Tests/WebStore.Services.Tests/Products/CartServiceTests.cs
+++ Tests/WebStore.Services.Tests/Products/CartServiceTests.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
 using WebStore.Domain.Models;
 using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
 using Assert = Xunit.Assert;
 
 namespace WebStore.Services.Tests.Products
@@ -11,6 +15,52 @@ namespace WebStore.Services.Tests.Products
     [TestClass]
     public class CartServiceTests
     {
+        private Cart _Cart;
+
+        private Mock<IProductData> _ProductDataMock;
+
+        private ICartService _CartService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _Cart = new Cart
+            {
+                Items = new List<CartItem>
+                {
+                    new CartItem { ProductId = 1, Quantity = 1 },
+                    new CartItem { ProductId = 2, Quantity = 3 }
+                }
+            };
+
+            _ProductDataMock = new Mock<IProductData>();
+            _ProductDataMock
+               .Setup(c => c.GetProducts(It.IsAny<ProductFilter>()))
+               .Returns(new List<ProductDTO>
+                {
+                    new ProductDTO
+                    {
+                        Id = 1,
+                        Name = "Product 1",
+                        Price = 1.1m,
+                        Order = 0,
+                        ImageUrl = "Product1.png",
+                        Brand = new BrandDTO { Id = 1, Name = "Brand 1" },
+                        Section = new SectionDTO { Id = 1, Name = "Section 1"}
+                    },
+                    new ProductDTO
+                    {
+                        Id = 2,
+                        Name = "Product 2",
+                        Price = 2.2m,
+                        Order = 0,
+                        ImageUrl = "Product2.png",
+                        Brand = new BrandDTO { Id = 2, Name = "Brand 2" },
+                        Section = new SectionDTO { Id = 2, Name = "Section 2"}
+                    },
+                });
+        }
+
         [TestMethod]
         public void Cart_Class_ItemsCount_returns_Correct_Quantity()
         {
