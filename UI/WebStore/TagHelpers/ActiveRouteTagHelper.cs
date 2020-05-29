239c61e7f4de59ee5b04be227eeee982c7bbe5f3
Infarh <shmachilin@yandex.ru>
2020-05-29 19:35:16 +0300

    Заготовка таг-хелпера подсветки активных ссылок

---
+++ UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers
+{
+    [HtmlTargetElement(Attributes = AttributeName)]
+    public class ActiveRouteTagHelper : TagHelper
+    {
+        private const string AttributeName = "is-active-route";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll(AttributeName);
+        }
+    }
+}
