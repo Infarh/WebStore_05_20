1ee394e5e849497ee3ac028ee60101f309a6b4de
Infarh <shmachilin@yandex.ru>
2020-05-29 20:27:51 +0300

    Заготовка визуального компонента для визуализации "хлебных крошек"

---
+++ Common/WebStore.Domain/ViewModels/BrandCompleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class BrandCompleteViewModel
+    {
+        public IEnumerable<BrandViewModel> Brands { get; set; }
+
+        public int? CurrentBrandId { get; set; }
+    }
+}
---
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class BreadCrumbsViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke() => View();
+    }
+}
