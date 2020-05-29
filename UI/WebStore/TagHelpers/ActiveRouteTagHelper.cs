69934588f362c3f4c8c009392ab3047a1e1a7e58
Infarh <shmachilin@yandex.ru>
2020-05-29 19:50:53 +0300

    Основная логика таг-хелпера подсветки активных ссылок

--- UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
+++ UI/WebStore/TagHelpers/ActiveRouteTagHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
@@ -18,7 +19,7 @@ namespace WebStore.TagHelpers
         public string Controller { get; set; }
 
         [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
-        public IDictionary<string, string> RouteValues { get; set; } 
+        public IDictionary<string, string> RouteValues { get; set; }
             = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
         [ViewContext, HtmlAttributeNotBound]
@@ -26,7 +27,48 @@ namespace WebStore.TagHelpers
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            if (IsActive())
+                MakeActive(output);
+
             output.Attributes.RemoveAll(AttributeName);
+        }
+
+        private bool IsActive()
+        {
+            var route_values = ViewContext.RouteData.Values;
+
+            var current_controller = route_values["controller"].ToString();
+            var current_action = route_values["action"].ToString();
+
+            const StringComparison ignore_case = StringComparison.OrdinalIgnoreCase;
+            if (!string.IsNullOrEmpty(Controller) && !string.Equals(current_controller, Controller, ignore_case))
+                return false;
+
+            if (!string.IsNullOrEmpty(Action) && !string.Equals(current_action, Action, ignore_case))
+                return false;
+
+            foreach (var (key, value) in RouteValues)
+                if (!route_values.ContainsKey(key) || route_values[key].ToString() != value)
+                    return false;
+
+            return true;
+        }
+
+        private static void MakeActive(TagHelperOutput output)
+        {
+            var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+            if (class_attribute is null)
+                output.Attributes.Add("class", "active");
+            else
+            {
+                if (class_attribute.Value.ToString()?.Contains("active") ?? false) return;
+
+                output.Attributes.SetAttribute("class",
+                    class_attribute.Value is null
+                        ? "active"
+                        : class_attribute.Value + " active");
+            }
         }
     }
 }
