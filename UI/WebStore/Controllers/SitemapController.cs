6eb1c2e652e65c3f00ff86e794c4e79f4487a63b
Infarh <shmachilin@yandex.ru>
2020-05-29 21:08:30 +0300

    Построение карты сайта

---
+++ UI/WebStore/Controllers/SitemapController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Controllers
+{
+    public class SitemapController : Controller
+    {
+        public IActionResult Index([FromServices] IProductData ProductData)
+        {
+            var nodes = new List<SitemapNode>
+            {
+                new SitemapNode(Url.Action("Index", "Home")),
+                new SitemapNode(Url.Action("ContactUs", "Home")),
+                new SitemapNode(Url.Action("Index", "Blogs")),
+                new SitemapNode(Url.Action("BlogSingle", "Blogs")),
+                new SitemapNode(Url.Action("Shop", "Catalog")),
+                new SitemapNode(Url.Action("Index", "WebAPITest")),
+            };
+
+            nodes.AddRange(ProductData.GetSections().Select(section => new SitemapNode(Url.Action("Shop", "Catalog", new { SectionId = section.Id }))));
+
+            foreach (var brand in ProductData.GetBrands())
+                nodes.Add(new SitemapNode(Url.Action("Shop", "Catalog", new { BrandId = brand.Id })));
+
+            foreach (var product in ProductData.GetProducts())
+                nodes.Add(new SitemapNode(Url.Action("Details", "Catalog", new { product.Id })));
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}
