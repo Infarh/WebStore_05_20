64013eb114785f173337ad60277a528fe8e1fcfc
Infarh <shmachilin@yandex.ru>
2020-06-02 19:18:50 +0300

    Добавлен визуальный компонент корзины

---
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
