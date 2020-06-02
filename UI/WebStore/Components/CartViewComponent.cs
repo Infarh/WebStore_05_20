d812988ad6509dd363cae37a48e3daa66dd30305
Infarh <shmachilin@yandex.ru>
2020-06-02 19:19:45 +0300

    Доработка логики визуального компонента корзины

--- UI/WebStore/Components/CartViewComponent.cs
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
 
 namespace WebStore.Components
 {
     public class CartViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly ICartService _CartService;
+
+        public CartViewComponent(ICartService CartService) => _CartService = CartService;
+
+        public IViewComponentResult Invoke() => View(_CartService.TransformFromCart());
     }
 }
