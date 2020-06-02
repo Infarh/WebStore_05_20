789174c8cd19dc16f58a3474a2d7d699762f8395
Infarh <shmachilin@yandex.ru>
2020-06-02 20:33:27 +0300

    Завершение логики скрипта асинхронного обновления корзины

--- UI/WebStore/wwwroot/js/Cart/CartLogik.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogik.js
@@ -6,20 +6,20 @@
         removeFromCartLink: ""
     },
 
-    init: function(properties) {
+    init: function (properties) {
         $.extend(Cart._properties, properties);
 
         Cart.initEvents();
     },
 
-    initEvents: function() {
+    initEvents: function () {
         $(".add-to-cart").click(Cart.addToCart);
         $(".cart_quantity_up").click(Cart.incrementItem);
-        //$(".cart_quantity_down").click(Cart.decrementItem);
-        //$(".cart_quantity_delete").click(Cart.removeFromCart);
+        $(".cart_quantity_down").click(Cart.decrementItem);
+        $(".cart_quantity_delete").click(Cart.removeFromCart);
     },
 
-    addToCart: function(event) {
+    addToCart: function (event) {
         event.preventDefault();
 
         var button = $(this);
@@ -27,30 +27,30 @@
 
         //fetch()
         $.get(Cart._properties.addToCartLink + "/" + id)
-            .done(function() {
+            .done(function () {
                 Cart.showToolTip(button);
                 Cart.refreshCartView();
             })
-            .fail(function() { console.log("addToCart fail"); });
+            .fail(function () { console.log("addToCart fail"); });
     },
 
-    showToolTip: function(button) {
+    showToolTip: function (button) {
         button.tooltip({ title: "Добавлено в корзину!" }).tooltip("show");
-        setTimeout(function() {
+        setTimeout(function () {
             button.tooltip("destroy");
         }, 500);
     },
 
-    refreshCartView: function() {
+    refreshCartView: function () {
         var container = $("#cart-container");
         $.get(Cart._properties.getCartViewLink)
-            .done(function(cartHtml) {
+            .done(function (cartHtml) {
                 container.html(cartHtml);
             })
             .fail(function () { console.log("refreshCartView fail"); });
     },
 
-    incrementItem: function(event) {
+    incrementItem: function (event) {
         event.preventDefault();
 
         var button = $(this);
@@ -59,7 +59,7 @@
         var container = button.closest("tr");
 
         $.get(Cart._properties.addToCartLink + "/" + id)
-            .done(function() {
+            .done(function () {
                 const count = parseInt($(".cart_quantity_input", container).val());
                 $(".cart_quantity_input", container).val(count + 1);
 
@@ -69,7 +69,7 @@
             .fail(function () { console.log("incrementItem fail"); });
     },
 
-    refreshPrice: function(container) {
+    refreshPrice: function (container) {
         const quantity = parseInt($(".cart_quantity_input", container).val());
         const price = parseFloat($(".cart_price", container).data("price"));
 
@@ -78,5 +78,57 @@
 
         $(".cart_total_price", container).data("price", totalPrice);
         $(".cart_total_price", container).html(value);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        var total = 0;
+
+        $(".cart_total_price").each(function () {
+            const price = parseFloat($(this).data("price"));
+            total += price;
+        });
+
+        var value = total.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+        $("#total-order-price").html(value);
+    },
+
+    decrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id"); // data-id="..."
+
+        var container = button.closest("tr");
+
+        $.get(Cart._properties.decrementLink + "/" + id)
+            .done(function () {
+                const count = parseInt($(".cart_quantity_input", container).val());
+                if (count > 1) {
+                    $(".cart_quantity_input", container).val(count - 1);
+                    Cart.refreshPrice(container);
+                } else {
+                    container.remote();
+                    Cart.refreshTotalPrice();
+                }
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("decrementItem fail"); });
+    },
+
+    removeFromCart: function(event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id"); // data-id="..."
+
+        $.get(Cart._properties.removeFromCartLink + "/" + id)
+            .done(function() {
+                button.closest("tr").remove();
+                Cart.refreshTotalPrice();
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("removeFromCart fail"); });
     }
 }
