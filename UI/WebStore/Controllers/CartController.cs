e1b238ff9bbdef4347490335782cb462196c846e
Infarh <shmachilin@yandex.ru>
2020-06-02 19:25:20 +0300

    ДОбавляем API-методы в контроллер корзины

--- UI/WebStore/Controllers/CartController.cs
+++ UI/WebStore/Controllers/CartController.cs
@@ -78,5 +78,35 @@ namespace WebStore.Controllers
             ViewBag.OrderId = id;
             return View();
         }
+
+        #region WebAPI
+
+        public IActionResult GetCartView() => ViewComponent("Cart");
+
+        public IActionResult AddToCartAPI(int id)
+        {
+            _CartService.AddToCart(id);
+            return Json(new { id, message = $"Товар id:{id} был добавлен в корзину" });
+        }
+
+        public IActionResult DecrementFromCartAPI(int id)
+        {
+            _CartService.DecrementFromCart(id);
+            return Ok();
+        }
+
+        public IActionResult RemoveFromCartAPI(int id)
+        {
+            _CartService.RemoveFromCart(id);
+            return Ok();
+        }
+
+        public IActionResult RemoveAllAPI()
+        {
+            _CartService.RemoveAll();
+            return Ok();
+        }
+
+        #endregion
     }
 }
