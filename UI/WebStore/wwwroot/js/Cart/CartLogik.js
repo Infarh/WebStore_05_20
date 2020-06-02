4123e543ea69600b26e9acfd93c567a5f4057791
Infarh <shmachilin@yandex.ru>
2020-06-02 19:46:38 +0300

    Заготовка скрипта корзины

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogik.js
@@ -0,0 +1,43 @@
+﻿Cart = {
+    _properties: {
+        getCartViewLink: "",
+        addToCartLink: ""
+    },
+
+    init: function(properties) {
+        $.extend(Cart._properties, properties);
+
+        $(".add-to-cart").click(Cart.addToCart);
+    },
+
+    addToCart: function(event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id"); // data-id="..."
+
+        //fetch()
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function() {
+                Cart.showToolTip(button);
+                Cart.refreshCartView();
+            })
+            .fail(function() { console.log("addToCart fail"); });
+    },
+
+    showToolTip: function(button) {
+        button.tooltip({ title: "Добавлено в корзину!" }).tooltip("show");
+        setTimeout(function() {
+            button.tooltip("destroy");
+        }, 500);
+    },
+
+    refreshCartView: function() {
+        var container = $("#cart-container");
+        $.get(Cart._properties.getCartViewLink)
+            .done(function(cartHtml) {
+                container.html(cartHtml);
+            })
+            .fail(function () { console.log("refreshCartView fail"); });
+    }
+}
