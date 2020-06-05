38d582a7204babf8b0e76a2b31a77fe7fa94e9b9
Infarh <shmachilin@yandex.ru>
2020-06-05 20:34:58 +0300

    Примеры использования ненавязчивого AJAX для формирования запросов к серверу

---
+++ UI/WebStore/ViewModels/AjaxTestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class AjaxTestViewModel
+    {
+        public int Id { get; set; }
+
+        public string Message { get; set; }
+        
+        public DateTime ServerTime { get; set; }
+    }
+}
